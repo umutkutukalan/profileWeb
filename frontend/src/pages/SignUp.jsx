49a45d0f574278972716c1df29abfaa506c384e1
Umut Kütükalan <118997325+umutkutukalan@users.noreply.github.com>
2024-09-21 06:09:33 +0300

    7-singUp-page-created

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const SignUp = () => {
+  const [username, setUsername] = useState();
+  const [mail, setMail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordRepeat, setPasswordRepeat] = useState();
+
+  return (
+    <section className="flex justify-center items-center">
+      <form className="container">
+        <div className="common-padding">
+          <div className="card">
+            <div className="card-header">
+              <div className="text-3xl">
+                <h1>Sign Up</h1>
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="mb-3">
+                <label htmlFor={username} className="form-label">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor={mail} className="form-label">
+                  Mail
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={(event) => setMail(event.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <labelh htmlFor={password} className="form-label">
+                  Password
+                </labelh>
+                <input
+                  type="password"
+                  className="form-control"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor={passwordRepeat} className="form-label">
+                  Password Repeat
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  onChange={(event) => setPasswordRepeat(event.target.value)}
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <button className="btn">Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SignUp;
