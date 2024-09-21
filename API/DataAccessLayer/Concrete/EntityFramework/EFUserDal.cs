2cc6659eedbb2dff750195ee340e78394f6f2d17
Ömer Şemsettin Uyanık <97348636+omersuyank@users.noreply.github.com>
2024-09-21 05:20:18 +0300

    Data AccessLayer Added

---
+++ API/DataAccessLayer/Concrete/EntityFramework/EFUserDal.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Concrete.Repository;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.EntityFramework
+{
+    public class EFUserDal : GenericRepository<User>, IUserDal
+    {
+        public EFUserDal(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ API/EntityLayer/Concrete/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string ProfileIMG { get; set; }
+        public string PasswordSalt { get; set; }
+        public int IsActive { get; set; }
+        
+    }
+}
